b71c3c25b5d5c66de6b8f37c43642ac68b4b9340
Thomas Malt <thomas@malt.no>
2018-05-14 23:44:13 +0200

    setting up prototype

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
 import React, { Component } from 'react';
 // import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
+import Slider from 'material-ui/Slider';
 import AppBar from 'material-ui/AppBar';
 import './App.css';
 
 // document.body.style = 'background: #00bcd4;';
 
-const SunscreenApp = () => (
-  <div style={{backgroundColor: "white"}}> 
-    <AppBar 
-      title="SD60 Sunscreen"
-      iconClassNameRight="muidocs-icon-navigation-expand-more"
-    />
-    <p>
-      <RaisedButton label="Default" />
-    </p>
-    <p>
-      Hello world
-    </p>
-  </div>
-);
+class SunscreenApp extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sunscreenValue: 0
+    }
+  };
+
+  onSliderChange = function(event, value) {
+    this.setState({
+      sunscreenValue: value
+    })
+  };
+
+  render() {
+    return (
+      <div style={{height: "80%"}}>
+        <AppBar 
+          title="SD60 Sunscreen"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
+        <div style={{
+          display: "flex",
+          flexDirection: "column",
+          backgroundColor: "white",
+          height: "100%"
+        }}>
+          <div style={{
+            height: "10vh",
+            flex: "0 0 auto",
+            textAlign: "center",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center"
+          }}>
+            <span style={{
+              display: "block",
+              fontSize: "8vh",
+              color: "#00bcd4"
+            }}>{this.state.sunscreenValue}</span>
+          </div>
+          <div style={{
+            flex: "1 0 auto",
+            width: "100%",
+            height: "72vh",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+
+            alignSelf: "center",
+            textAlign: "center"
+          }}>
+            <Slider 
+              style={{
+                height: "90%"
+              }} 
+             axis="y" 
+             defaultValue={this.state.sunscreenValue} 
+             min={0}
+             max={100}
+             step={1}
+             onChange={this.onSliderChange.bind(this)}
+            />
+          </div>
+          <div style={{
+            height: "10.10vh",
+            flex: "0 0 auto"
+          }}>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
